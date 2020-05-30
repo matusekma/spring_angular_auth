36dd703fed451405a29effb13005a74180846ede
Marci <matusekma@gmail.com>
2020-05-30 15:09:10 +0200

    Change front page

--- src/main/java/hu/bme/aut/spring_angular_auth/SpringAngularAuthApplication.java
+++ src/main/java/hu/bme/aut/spring_angular_auth/SpringAngularAuthApplication.java
@@ -2,9 +2,24 @@ package hu.bme.aut.spring_angular_auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 @SpringBootApplication
+@RestController
 public class SpringAngularAuthApplication {
+
+    @GetMapping("/resource")
+    public Map<String, Object> home() {
+        Map<String, Object> model = new HashMap<>();
+        model.put("id", UUID.randomUUID().toString());
+        model.put("content", "Hello World");
+        return model;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringAngularAuthApplication.class, args);
