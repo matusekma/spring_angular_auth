f51372a9c3d4408df55efcde519311cb871d6a8d
Marci <matusekma@gmail.com>
2020-05-30 13:03:46 +0200

    Initial commit

---
+++ src/main/java/hu/bme/aut/spring_angular_auth/SpringAngularAuthApplication.java
@@ -0,0 +1,13 @@
+package hu.bme.aut.spring_angular_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAngularAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAngularAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hu/bme/aut/spring_angular_auth/SpringAngularAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.bme.aut.spring_angular_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAngularAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
